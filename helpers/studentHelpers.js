94bd611bbd1e7e20065d3147e6392db80a267654
jafar-ctcl <jafarctcl@gmai.com>
2024-12-01 09:23:14 +0530

    admin approve added

--- helpers/studentHelpers.js
+++ helpers/studentHelpers.js
@@ -1,60 +1,72 @@
 let db = require('../config/connection')
 
-module.exports={
+module.exports = {
     // doSignup:(userData)={
     //     return new Promise((resolve, reject) => {
     //         db.query('insert into students values ')
-          
+
     //     })
     // }
- // Make sure you have a MySQL connection pool or client setup
+    // Make sure you have a MySQL connection pool or client setup
 
- doSignup : (loginData) => {
-    const { name,type, course ,year, email, password,aadhar,gender} = loginData;
-    return new Promise((resolve, reject) => {
-        // Define the SQL query
-        // const query = 'INSERT INTO students (name,type, course ,year, email, password,gender) VALUES (?, ?, ?, ?, ?,?,?)';
-        // Execute the query
-        db.query( 'INSERT INTO login_data (name,type, course ,year, email, password,aadhar,gender) VALUES (?, ?, ?, ?, ?,?,?,?)', [name,type, course ,year, email,  password,aadhar,gender], (err, results) => {
-            if (err) {
-                console.error('Error inserting into database:',err);
-                throw err
-                // Reject the promise on error
-            } 
-            resolve()
+    doSignup: (loginData) => {
+        const { name, type, course, year, phone, email, password, aadhar, gender } = loginData;
+        return new Promise((resolve, reject) => {
+            // Define the SQL query
+            // const query = 'INSERT INTO students (name,type, course ,year, email, password,gender) VALUES (?, ?, ?, ?, ?,?,?)';
+            // Execute the query
+            db.query('INSERT INTO login_data (name,type, course ,year,phone, email, password,aadhar,gender) VALUES (?, ?, ?, ?, ?,?,?,?,?)', [name, type, course, year, phone, email, password, aadhar, gender, 0], (err, results) => {
+                if (err) {
+                    console.error('Error inserting into database:', err);
+                    throw err
+                    // Reject the promise on error
+                }
+                resolve()
+            });
         });
-    });
-},
-doLogin :(loginData)=>{
+    },
+    doLogin: (loginData) => {
 
-    // const email = loginData.Email
-    // const password = loginData.Password
-   
-    // const { email, password } = userData;
+        // const email = loginData.Email
+        // const password = loginData.Password
 
-    return new Promise((resolve, reject) => {
-        // Query the database for the user by email
-        const query = 'SELECT * FROM login_data WHERE email = ?';
-        db.query('SELECT * FROM login_data WHERE email = ?', [loginData.email], (err, data) => {
-            // console.log(data[0]);
-           
-            if (data.length === 0) {
-                // No user found with the provided email
-            //   console.log("email not exist");``
-                
-                resolve({err:'Email not exist'})
-            }else{
-                // if(userData.email==data[0].Email){
-                    if(loginData.password == data[0].Password){
-                    //    console.log("pasword is equal");
-                          resolve({err:false})
-                    }else{
-                        resolve({err:'Password is incorrect'})
+        // const { email, password } = userData;
+
+        return new Promise((resolve, reject) => {
+            // Query the database for the user by email
+            const query = 'SELECT * FROM login_data WHERE email = ?';
+            db.query('SELECT * FROM login_data WHERE email = ?', [loginData.email], (err, data) => {
+                // console.log(data[0]);
+
+                if (data.length === 0) {
+                    // No user found with the provided email
+                    //   console.log("email not exist");``
+
+                    resolve({ err: 'Email not exist' })
+                } else {
+
+                    if (loginData.password == data[0].Password) {
+                        console.log(data[0].status);
+                        if (data[0].Type === "student") {
+                            if (data[0].status) {
+
+                                resolve({ err: false, data })
+                            } else {
+
+                                resolve({ err: "Account is inactive" })
+                            }
+                            //  console.log(".................");
+                        } else {
+                            resolve({ err: "Email not exist" })
+                        }
+
+                    } else {
+                        resolve({ err: 'Password is incorrect' })
                     }
-                    
-            }
+
+                }
+            })
         })
-    }) 
+    }
 }
-}    
 
