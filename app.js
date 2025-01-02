231e2392e2bd783a921c4dc67afe0e6f50671e93
jafar-ctcl <jafarctcl@gmai.com>
2025-01-02 14:42:14 +0530

    if eq added

--- app.js
+++ app.js
@@ -4,53 +4,64 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var studentRouter = require('./routes/student');
 var hodRouter = require('./routes/hod');
 var teacherRouter = require('./routes/teacher');
 
 var hbs = require('express-handlebars');
-// var handlebars = require('handlebars')
 const db = require('./config/connection');
 const session = require('express-session');
 var app = express();
+var hbs = require('express-handlebars');
+
+// Register eq helper globally
+hbs.create({
+  helpers: {
+    eq: (a, b) => a === b,
+  },
+});
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',
+app.engine(
+  'hbs',
   hbs.engine({
-    extname:'hbs',
-    defaultLayout:'layout',
-    layoutsDir:__dirname+'/views/layout/',
-    partialsDir:__dirname+'/views/partials/'
-  }))
-//db connection
-db.connect((err)=>{
-  if(err) throw err;
-  console.log('Db Connected');
-  
-})
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layout/',
+    partialsDir: __dirname + '/views/partials/',
+    helpers: {
+      eq: (a, b) => a === b, // Ensure eq helper is registered here
+    },
+  })
+);
 
+
+// db connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Db Connected');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:"dms",cookie:{maxAge:600000}}))
+app.use(session({ secret: 'dms', cookie: { maxAge: 600000 } }));
 app.use('/', studentRouter);
 app.use('/hod', hodRouter);
 app.use('/teacher', teacherRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -60,6 +71,70 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-
 module.exports = app;
+
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+
+// var studentRouter = require('./routes/student');
+// var hodRouter = require('./routes/hod');
+// var teacherRouter = require('./routes/teacher');
+
+// var hbs = require('express-handlebars');
+// // var handlebars = require('handlebars')
+// const db = require('./config/connection');
+// const session = require('express-session');
+// var app = express();
+
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+// app.engine('hbs',
+//   hbs.engine({
+//     extname:'hbs',
+//     defaultLayout:'layout',
+//     layoutsDir:__dirname+'/views/layout/',
+//     partialsDir:__dirname+'/views/partials/'
+//   }))
+// //db connection
+// db.connect((err)=>{
+//   if(err) throw err;
+//   console.log('Db Connected');
+  
+// })
+
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session({secret:"dms",cookie:{maxAge:600000}}))
+// app.use('/', studentRouter);
+// app.use('/hod', hodRouter);
+// app.use('/teacher', teacherRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
+
+// module.exports = app;
