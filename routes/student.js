2200c4efc196526e1ec29e9e81ea9f4b0fbfe051
jafar-ctcl <jafarctcl@gmai.com>
2025-01-31 07:21:16 +0530

    login and signup created

--- routes/student.js
+++ routes/student.js
@@ -8,18 +8,25 @@ const verifyLogin=(req,res,next)=>{
   if(req.session.loggedIn == true){
     next()
   }else{
+   
     res.redirect('/login')
   }
 }
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title:'SASC' ,student });
+router.get('/',(req, res)=> {
+  res.render('index', {title:'SASC'});
+});
+router.get('/student',verifyLogin,(req, res)=> {
+  let name =  req.session.student.name
+  console.log(name);
+  
+  res.render('student/dashboard', {student,name });
 });
 router.get('/login', function (req, res) {
   if (req.session.loggedIn) {
-    res.redirect('/student')
+    res.redirect('/login')
   } else {
-    res.render('login', { title: 'Student', err: req.session.loginErr,student });
+    res.render('student/login', { title: 'Student', err: req.session.loginErr });
    // req.session.loginErr = false
   }
 });
@@ -39,44 +46,22 @@ router.post('/login', (req, res) => {
     }
   })
 })
-
-// router.post('/login', (req, res) => {
-//   studentHelpers.doLogin(req.body).then((resp) => {
-//     if (resp.err) {
-//       req.session.loginErr = resp.err
-//       res.redirect('/login')
-//     } else {
-//       req.session.loggedIn = true
-//       req.session.student = resp.name
-//       console.log(resp.data);
-      
-//       res.redirect('/student')
-//     }
-//   })
-// })  
 router.get('/signup',(req,res)=>{
-  res.render('signup')
+  res.render('student/signup')
 })
-// router.post('/signup',(req,res)=>{
-//   console.log(req.body);
-//   studentHelpers.doSignup(req.body).then(()=>{
-//         res.redirect('/login')
-//   })
-// })
 router.post('/signup', (req, res) => {
   console.log(req.body);
-  studentHelpers
-      .doSignup(req.body)
-      .then(() => {
+  studentHelpers.doSignup(req.body).then(() => {
           res.redirect('/login'); // Redirect to the login page on success
       })
       .catch((error) => {
           console.error('Signup error:', error);
+          console.log("error",error.message);
+          
           // Render the signup page with the error message
-          res.render('/signup', { result: error.message });
+          res.render('student/signup', { error: error.message });
       });
 });
-
 router.get('/logout', (req, res) => {
   req.session.destroy()
   res.redirect('/student')
@@ -84,18 +69,14 @@ router.get('/logout', (req, res) => {
 router.get('/student',verifyLogin,(req,res)=>{
   // console.log(req.session.student[0]);
   let name = req.session.student.name
-  
   res.render('student/dashboard',{name})
 })
-router.get('/timetable',(req,res)=>{
-  
-  res.render('student/dashboard',{student})
-})
+
 router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-// router.get('/teacher',(req,res)=>{
-//   res.render('teacher/view-attendence')
-// })
+router.get("/timetable",(req,res)=>{
+  res.render("student/view-timetable")
+})
 module.exports = router;
